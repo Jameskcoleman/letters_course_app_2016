bf02b58b927f2788650a48998ee1c52ba8f96846
James Coleman <james.k.coleman@gmail.com>
2014-10-19 06:49:55 -0400

    Added users controller and spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,4 @@ class PagesController < ApplicationController
   def front
     redirect_to home_path if current_user
   end
-
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
-  end
-
 end
