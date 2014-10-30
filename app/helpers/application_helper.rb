53942631f04b03cdafb19d8b23a4fc55ac0030a5
James Coleman <james.k.coleman@gmail.com>
2014-10-30 14:04:12 -0400

    Added links from comment to what the comment is about in Portfolio view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def is_active?(link_path)
-   current_page?(link_path) ? "active" : ""
-  end
+def is_active?(link_path)
+  "active" if request.fullpath.include? link_path
+end
 
   def display_datetime(dt)
     dt.in_time_zone('Eastern Time (US & Canada)').strftime("%m/%d/%Y %l:%M%P %Z") # 03/14/2013 9:09pm
