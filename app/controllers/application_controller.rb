74f56dd627b048a62af06dae2ca31af2dcb80a15
James Coleman <james.k.coleman@gmail.com>
2014-10-30 15:59:52 -0400

    Added ability to edit posts and letters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+  helper_method :logged_in?
+
   def require_user
     redirect_to sign_in_path unless current_user
   end
@@ -11,6 +14,17 @@ class ApplicationController < ActionController::Base
     User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def logged_in?
+    !!current_user
+  end
+
+  def require_admin
+    access_denied unless logged_in? and current_user.admin?
+  end
+
+  def access_denied
+    flash[:error] = "You can't do that."
+    redirect_to root_path
+  end
 
 end
